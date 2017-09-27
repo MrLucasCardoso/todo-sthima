3812bc5102b24f8e0b4042da2408dbb805a1506f
Lucas Cardoso <mr.lucascardoso@gmail.com>
2017-09-27 00:12:45 -0300

    Adicionado model-mommy para auxiliar nos testes

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from core.models import Todo
+from model_mommy import mommy
 
-# Create your tests here.
+
+class TestTodo(TestCase):
+    """
+    Classe de teste para operações com model Todo
+    """
+
+    def setUp(self):
+        self.todo = mommy.make(Todo, _fill_optional=True)
+        self.todos = mommy.make(Todo, _fill_optional=True, _quantity=3)
+
+    def test_foo(self):
+        self.assertTrue(True)
+
